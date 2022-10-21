d271d9a31d70c60e0ee92e8bf4866717f0c0f253
zzy <zzy@qq.com>
2022-10-21 16:18:16 +0800

    hot-fix commit1

--- src/main/java/com/example/wind/A.java
+++ src/main/java/com/example/wind/A.java
@@ -7,5 +7,6 @@ public class A {
         System.out.println("first 第一次");
         System.out.println("second 第二次");
         System.out.println("third 第三次");
+        System.out.println("hello git4");
     }
 }
