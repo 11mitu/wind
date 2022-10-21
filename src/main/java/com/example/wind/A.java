dc1d22449ef2b6d4a6a2e6e88672a0175542f98d
zzy <zzy@qq.com>
2022-10-21 16:05:25 +0800

    third commit

--- src/main/java/com/example/wind/A.java
+++ src/main/java/com/example/wind/A.java
@@ -6,5 +6,6 @@ public class A {
     public static  void main(String[] args) {
         System.out.println("first 第一次");
         System.out.println("second 第二次");
+        System.out.println("third 第三次");
     }
 }
